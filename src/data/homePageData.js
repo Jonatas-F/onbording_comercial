317ee85326f5ca94223fba8f6c9a6296a87e445c
Unlucky-Jon <jonatas.freire.p@gmail.com>
2026-04-03 17:35:15 -0300

    feat: centraliza dados da HomePage

---
+++ src/data/homePageData.js
@@ -0,0 +1,70 @@
+export const highlightsSection = {
+  eyebrow: 'Destaques',
+  title: 'Blocos reorganizáveis',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.',
+  cards: [
+    {
+      eyebrow: 'Texto',
+      title: 'Conteúdo em módulos',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.',
+    },
+    {
+      eyebrow: 'Imagem',
+      title: 'Cards visuais independentes',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum.',
+    },
+    {
+      eyebrow: 'Vídeo',
+      title: 'Blocos flexíveis por resolução',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur.',
+    },
+  ],
+}
+
+export const modulesSection = {
+  eyebrow: 'Estrutura',
+  title: 'Módulos do portal',
+  cards: [
+    {
+      eyebrow: '01',
+      title: 'Onboarding',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      eyebrow: '02',
+      title: 'Processo de vendas',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      eyebrow: '03',
+      title: 'CRM Sankhya',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      eyebrow: '04',
+      title: 'Técnicas de venda',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      eyebrow: '05',
+      title: 'Objeções',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      eyebrow: '06',
+      title: 'Indicadores e metas',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      eyebrow: '07',
+      title: 'Boas práticas',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      eyebrow: '08',
+      title: 'Biblioteca',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+  ],
+}
