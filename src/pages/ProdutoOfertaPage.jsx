d07d2ecda815d5689e6f49de81cb65f9c3031f8d
Unlucky-Jon <jonatas.freire.p@gmail.com>
2026-04-03 17:52:52 -0300

    feat: cria pagina de produto e oferta

--- src/pages/ProdutoOfertaPage.jsx
+++ src/pages/ProdutoOfertaPage.jsx
@@ -1,3 +1,39 @@
-import SiteLayout from '../components/layout/SiteLayout'
-import ProdutoOfertaModule from '../modules/produto-oferta/ProdutoOfertaModule'
-export default function ProdutoOfertaPage(){return <SiteLayout><ProdutoOfertaModule /></SiteLayout>}
+import ModulePageTemplate from '../components/blocks/ModulePageTemplate'
+
+const produtoOfertaBlocks = [
+  {
+    eyebrow: 'Bloco 01',
+    title: 'Entendimento do portfólio',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum.',
+  },
+  {
+    eyebrow: 'Bloco 02',
+    title: 'Construção da oferta',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed odio dui. Curabitur blandit tempus porttitor. Nulla vitae elit libero, a pharetra augue.',
+  },
+  {
+    eyebrow: 'Bloco 03',
+    title: 'Apresentação de valor',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
+  },
+]
+
+export default function ProdutoOfertaPage() {
+  return (
+    <ModulePageTemplate
+      eyebrow="Produto e oferta"
+      title="Apresentação comercial, valor percebido e construção da proposta"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+      introLabel="Panorama"
+      introTitle="Visão geral de produto e oferta"
+      introText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper."
+      contentTitle="Estrutura da oferta"
+      contentDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet."
+      textBlocks={produtoOfertaBlocks}
+      videoEyebrow="Vídeo"
+      videoTitle="Vídeo de produto e oferta"
+      videoDescription="Espaço reservado para um vídeo mostrando como apresentar diferenciais, organizar argumentos de valor e estruturar a oferta comercial."
+      videoPlaceholderLabel="Vídeo de produto e oferta"
+    />
+  )
+}
